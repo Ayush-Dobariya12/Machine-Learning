0b4f6d432d4bc527a8cd1d070b61b5169a3f71ea
Ayush-Dobariya <ayushdobariya2026@gmail.com>
2026-05-22 09:40:49 +0530

    Add files via upload

---
+++ Machine Learning/Python Course/Chapter 4/Prectice Set/01_problem.py
@@ -0,0 +1,9 @@
+dict = {
+    "Billi":"Cat",
+    "Kaichi":"Seasors",
+    "Gaadi":"Car"
+}
+
+i = input("Enter Hindi Word:")
+
+print(f"Your English Word is {dict[i]}")
---
+++ Machine Learning/Python Course/Chapter 4/Prectice Set/04_problem.py
@@ -0,0 +1,8 @@
+dict = {}
+
+dict.update({"Ayush":"Java"})
+dict.update({"Kevin":"Python"})
+dict.update({"Bhavy":"Python"})
+dict.update({"Jatan":"Python"})
+
+print(dict)
